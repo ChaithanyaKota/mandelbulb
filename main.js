d86ce25c32c3bb6366c193c92535e3470795c383
ChaithanyaKota <chaithanya.kota007@gmail.com>
2024-01-23 19:24:54 -0600

    Shader Initializer function

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 main();
+initShaderProgram();
+loadShader();
 
 function main() { 
     const canvas = document.querySelector("#canvas");
@@ -8,9 +10,65 @@ function main() {
         return; 
     }
 
+    const vs = `
+    attribute vec4 aVertexPosition;
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+    void main() {
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    }
+  `;
+
+  const fs = `
+  void main() {
+    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+  }
+  `;
+
+  const shaderProgram = initShaderProgram(gl, vs, fs);
+  
+
+
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
     gl.clear(gl.COLOR_BUFFER_BIT);
 
+}
+
+function initShaderProgram(gl, vsSource, fsSource) { 
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+    // create shader program
+     const shaderProgram = gl.createProgram();
+     gl.attachShader(shaderProgram, vertexShader);
+     gl.attachShader(shaderProgram, fragmentShader);
+     gl.linkProgram(shaderProgram);
+
+
+     // if creating the shader program fails
+     if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        return null;
+      };
+
+    return shaderProgram; 
+}
+
+function loadShader(gl, type, source) { 
+    const shader = gl.createShader(type);
+
+  // send source to the shader object
+  gl.shaderSource(shader, source);
+
+  // compile shader program
+  gl.compileShader(shader);
+
+  // see if it compiled successfully
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
 }
 
