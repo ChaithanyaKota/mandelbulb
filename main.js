7a5416130246228352e9bc12512f23966e5aa059
ChaithanyaKota <chaithanya.kota007@gmail.com>
2024-01-23 19:42:46 -0600

    initializing buffers

--- main.js
+++ main.js
@@ -2,15 +2,16 @@ main();
 initShaderProgram();
 loadShader();
 
-function main() { 
-    const canvas = document.querySelector("#canvas");
+function main() {
+  const canvas = document.querySelector("#canvas");
 
-    const gl = canvas.getContext("webgl");
-    if(!gl) { 
-        return; 
-    }
+  const gl = canvas.getContext("webgl");
+  if (!gl) {
+    return;
+  }
 
-    const vs = `
+  // Vertex Shader
+  const vs = `
     attribute vec4 aVertexPosition;
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
@@ -19,43 +20,59 @@ function main() {
     }
   `;
 
+  // Fragment shader
   const fs = `
   void main() {
     gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
   }
   `;
 
+  // initializing shader
   const shaderProgram = initShaderProgram(gl, vs, fs);
+
+
+  
+  const programInfo = {
+    program: shaderProgram,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+    },
+    uniformLocations: {
+      projectionMatrix: gl.getUniformLocation(
+        shaderProgram,
+        "uProjectionMatrix"
+      ),
+      modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+    },
+  };
+
   
 
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
+  gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
-function initShaderProgram(gl, vsSource, fsSource) { 
-    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
-    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+function initShaderProgram(gl, vsSource, fsSource) {
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
 
-    // create shader program
-     const shaderProgram = gl.createProgram();
-     gl.attachShader(shaderProgram, vertexShader);
-     gl.attachShader(shaderProgram, fragmentShader);
-     gl.linkProgram(shaderProgram);
+  // create shader program
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
 
+  // if creating the shader program fails
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    return null;
+  }
 
-     // if creating the shader program fails
-     if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        return null;
-      };
-
-    return shaderProgram; 
+  return shaderProgram;
 }
 
-function loadShader(gl, type, source) { 
-    const shader = gl.createShader(type);
+function loadShader(gl, type, source) {
+  const shader = gl.createShader(type);
 
   // send source to the shader object
   gl.shaderSource(shader, source);
@@ -71,4 +88,3 @@ function loadShader(gl, type, source) {
 
   return shader;
 }
-
